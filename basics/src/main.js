cb2084305354e23c4f77c19992ae297eee8b6170
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-12-05 18:18:59 -0300

    add value to array

--- basics/src/main.js
+++ basics/src/main.js
@@ -14,7 +14,9 @@ const checkValue = (value) => {
 }
 
 const myArray = Array.from([1, 2, 3], x => x + x);
-console.log(myArray);
+console.log(...myArray, 4);
+console.log(myArray.push(5))
+console.log(myArray.unshift(6))
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Hello World!</h1>
@@ -41,4 +43,7 @@ document.querySelector('#app').innerHTML = `
     <p>"undefined:", checkValue(undefined) -> Uncaught Error: The value is invalid!, Try again.</p>
     <p>"null:", checkValue(null) -> Uncaught Error: The value is invalid!, Try again.</p>
     <p>"Array.from([1, 2, 3], x => x + x): ${myArray}"</p>
+    <p>"[...myArray, 4]: ${[...myArray, 4]}"</p>
+    <p>"myArray.push(5), Appends new elements to the end of an array: ${myArray.push(5)}"</p>
+    <p>"myArray.unshift(6), Inserts new elements at the start of an array: ${myArray.unshift(6)}"</p>
   `
