fa4de1f1a5acc5a8aad88a1d2bcb256092d82c8f
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-12-05 15:52:23 -0300

    chore: Boolean operations

--- basics/src/main.js
+++ basics/src/main.js
@@ -12,5 +12,18 @@ document.querySelector('#app').innerHTML = `
     <h1>Hello World!</h1>
     <p>JavaScript was announced ${formatter.format(diffInDays, 'days')}.</p>
     <p>It has been ${formatter.format(diffInYear, 'year')} years since its announcement.</p>
+    <p>"String:", ${ Boolean("Ulises") }</p>
+    <p>"1235:", ${ Boolean(1235)}</p>
+    <p>"-1235:", ${ Boolean(-1235)}</p>
+    <p>"Object:", ${ Boolean({text: "hi"})}</p>
+    <p>"Array:", ${ Boolean(["apple", -1, false])}</p>
+    <p>"Function:", ${ Boolean(function(){})}</p>
+    <p>"Arrow function:", ${ Boolean(() => {})}</p>
+    <p>"Empty string:", ${ Boolean("") }</p>
+    <p>"0:", ${ Boolean(0)}</p>
+    <p>"-0:", ${ Boolean(-0)}</p>
+    <p>"null:", ${ Boolean(null)}</p>
+    <p>"undefined:", ${ Boolean(undefined)}</p>
+    <p>"NaN:", ${ Boolean(NaN)}</p>
   </div>
 `
