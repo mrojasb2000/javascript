d8f2b7e5ebc7e06c97790eeec70f2e65e3fd6ae0
Mauricio Rojas <ext_mauricio.rojasb@tapp.cl>
2025-12-05 15:31:02 -0300

    chore: initial commit

---
+++ basics/src/main.js
@@ -0,0 +1,21 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+const jsDateAnnouncement = new Date(818031600000);
+const currentDate = new Date();
+const diff = jsDateAnnouncement - currentDate;
+const formatter = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+const diffInDays = Math.round(diff / 86400000);
+const diffInYear = Math.round(diffInDays / 365);
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello World!</h1>
+    <p>JavaScript was announced ${formatter.format(diffInDays, 'days')} ago.</p>
+    <p>It has been ${formatter.format(diffInYear, 'year')} years since its announcement.</p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
